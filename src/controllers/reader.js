4f492a8e68e80158ce6fa24a2d9b051ff1d101ce
isabellacopland <isabella.copland@gmail.com>
2022-06-16 22:01:57 +0100

    CRUD completed

--- src/controllers/reader.js
+++ src/controllers/reader.js
@@ -5,6 +5,51 @@ exports.create = async (req, res) => {
   res.status(201).json(newReader);
 };
 
+exports.findAll = async (req, res) => {
+    const readers = await Reader.findAll();
+    res.status(200).json(readers);
+};
+
+exports.findByPk = async (req, res) => {
+    const { id } = req.params;
+
+    const reader = await Reader.findByPk(id);
+
+    if(!reader) {
+        res.status(404).json({ error: 'The reader could not be found.' });
+    } else {
+        res.status(200).json(reader);
+    }
+
+};
+
+exports.update = async (req, res) => {
+    const { id } = req.params;
+    const updateData = req.body;
+
+    const [ updatedRows ] = await Reader.update(updateData, {where: {id} });
+
+    if(!updatedRows) {
+        res.status(404).json({ error: 'The reader could not be found.' });
+    } else {
+        res.status(200).json(updatedRows)
+    }
+};
+
+exports.destroy = async (req, res) => {
+    const { id } = req.params;
+    
+    const deletedRows = await Reader.destroy({ where: { id }});
+
+    if(!deletedRows) {
+        res.status(404).json({ error: 'The reader could not be found.' });
+    } else {
+    res.status(204).json(deletedRows);
+    }
+};
+
+
+
 
 
 
--- src/routes/reader.js
+++ src/routes/reader.js
@@ -6,4 +6,12 @@ const router = express.Router();
 
 router.post('/', readerController.create);
 
+router.get('/', readerController.findAll);
+
+router.get('/:id', readerController.findByPk);
+
+router.patch('/:id', readerController.update);
+
+router.delete('/:id', readerController.destroy);
+
 module.exports = router;
